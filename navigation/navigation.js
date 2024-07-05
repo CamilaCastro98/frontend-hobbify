79e8b1052afe694b8656b715b8d67bf7851673af
Camila Castro <cami@Camilas-MacBook-Air.local>
2024-07-05 15:12:34 -0300

    Modificación para que funcione la redirección a la app luego de hacer pagos en stripe

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -12,30 +12,47 @@ import SubscriptionScreen from "../src/screens/SubscriptionScreen";
 import Landing from "../src/screens/Landing";
 import CreateHobby from "../src/screens/CreateHobby";
 import SubmitedHobby from "../src/screens/SubmitedHobby";
+import SuccessScreen from "../src/screens/SuccessScreen";
+import CancelScreen from "../src/screens/CancelScreen";
+import * as Linking from 'expo-linking'
+import DeepLinkingHandler from "../src/helpers/deepLinkingHandler";
 const Stack = createStackNavigator();
 const screenOptions = {
   headerShown: false,
 };
 
-const AppNavigation = () => (
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Landing" screenOptions={screenOptions}>
-    <Stack.Screen name="Login" component={Login}></Stack.Screen>
-      <Stack.Screen name="HobbySelector" component={HobbySelector}></Stack.Screen>
-      <Stack.Screen name="Messages" component={Messages}></Stack.Screen>
-      <Stack.Screen name="Chat" component={Chat}></Stack.Screen>
-      <Stack.Screen name="MainFeed"component={MainFeed}></Stack.Screen>
-      <Stack.Screen name="Perfil" component={Perfil}></Stack.Screen>
-      <Stack.Screen name="Register" component={Register}></Stack.Screen>
-      <Stack.Screen name="Landing" component={Landing}></Stack.Screen>
-      <Stack.Screen name="CreateHobby" component={CreateHobby}></Stack.Screen>
-      <Stack.Screen name="SubmitedHobby" component={SubmitedHobby}></Stack.Screen>
-      <Stack.Screen
-        name="SubscriptionScreen"
-        component={SubscriptionScreen}
-      ></Stack.Screen>
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+const linking = {
+  prefixes: [Linking.createURL('/')],
+  config: {
+    screens: {
+      SuccessScreen: 'success',
+      CancelScreen: 'cancel',
+    },
+  },
+}
+
+const AppNavigation = () => {
+
+  return (
+    <NavigationContainer linking={linking}>
+      <DeepLinkingHandler />
+      <Stack.Navigator initialRouteName="Landing" screenOptions={screenOptions}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="HobbySelector" component={HobbySelector} />
+        <Stack.Screen name="Messages" component={Messages} />
+        <Stack.Screen name="Chat" component={Chat} />
+        <Stack.Screen name="MainFeed" component={MainFeed} />
+        <Stack.Screen name="Perfil" component={Perfil} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Landing" component={Landing} />
+        <Stack.Screen name="CreateHobby" component={CreateHobby} />
+        <Stack.Screen name="SubmitedHobby" component={SubmitedHobby} />
+        <Stack.Screen name="SubscriptionScreen" component={SubscriptionScreen} />
+        <Stack.Screen name="SuccessScreen" component={SuccessScreen} />
+        <Stack.Screen name="CancelScreen" component={CancelScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default AppNavigation;
